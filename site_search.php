a24d3276cc635d073a18b5ea8ad1370a5a5b37cc
Anthony Hartup <anth@anthscomputercave.com>
2018-10-09 12:39:17 +1100

    New options
    Added option to call Python search program if moved outside program folder.

--- site_search.php
+++ site_search.php
@@ -2,7 +2,10 @@
 // Call the AAIMI Site Search Python program with the search terms as parameters. 
 // It will return a HTML-formatted list of pages containing the most instances of the search terms
 $terms = $_POST['searchterms'];
+// Comment out the following two lines to call the Python program in a custom location
 $dir = exec("pwd");
 $command = "python " . $dir . "/site_search.py " . $terms;
+// Uncomment the following two lines to call the Python program in a custom location. Change home/to/path to full path.
+//$command = "python /home/path/to/site_search.py " . $terms;
 system($command);
 ?>
